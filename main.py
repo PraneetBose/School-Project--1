af250f2d144047c6a48cf7abad04baa6bec4433e
Praneet Bose <94593299+PraneetBose@users.noreply.github.com>
2021-12-26 01:43:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,10 @@
 #  net amount of products after 10% discount on the articles
 
 # storing the amount of 1 Article in  "num"
-num = int(input("cost of 1 articles is = "))
+num = float(input("cost of 1 articles is = "))
 
 # storing the number of articles in x and the multiplying it with the amount of 1 article
-x = int(input("Quantity of articles - "))
+x = float(input("Quantity of articles - "))
 print("Total price of the Articles=", x * num)
 # storing the value of x*num in y
 y = x * num
